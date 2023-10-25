005c3cc052fd3d99391db4cd222ee50fed2a5d8b
1SeongMin <118499294+1SeongMin@users.noreply.github.com>
2023-10-26 03:33:39 +0900

    [Silver IV] Title: 수들의 합 2, Time: 152 ms, Memory: 14972 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -9,12 +9,13 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine()); //공백으로 문자열 구분
 
+        int cnt = 0;
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int A[] = new int [N];
 
-        //배열의 숫자 입력받기
         StringTokenizer st2 = new StringTokenizer(br.readLine());
+        //배열의 숫자 입력받기
         for(int i=0; i<N; i++) {
             A[i] = Integer.parseInt(st2.nextToken());
         }
@@ -22,16 +23,19 @@ public class Main {
         //M이 되는 경우의 수 구하기
         int start = 0;
         int end = 0;
-        int preSum = 0;
-        int cnt = 0;
+        int sum = 0;
 
         while(true) {
-            if (preSum >= M) {
-                if (preSum == M) cnt++;
-                preSum -= A[start++];
+            if (sum >= M) { //연속된 배열의 합을 구하기 위해 같을 때도 빼줌
+                if (sum == M) cnt++; //M과 sum 같으면 cnt 증가
+                sum -= A[start];
+                start++;
+            } else if 
+                (end == N) break;
+            else {
+                sum += A[end];
+                end++;
             }
-            else if (end == N) break;
-            else preSum += A[end++];
         }
         System.out.println(cnt);
     }
